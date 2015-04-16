c7ab24403d8d5614138ab0746c068990c3b0b198
Théo Guerin <theo.guerin.pro@gmail.com>
2015-04-16 23:09:32 +0000

    register de silex a l'application

--- app/app.php
+++ app/app.php
@@ -1,1 +1,12 @@
 <?php
+
+use Symfony\Component\Debug\ErrorHandler;
+use Symfony\Component\Debug\ExceptionHandler;
+
+// Register global error and exception handlers
+ErrorHandler::register();
+ExceptionHandler::register();
+
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/../views',
+));
