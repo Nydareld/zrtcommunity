dfc1bfc036b06614956bd9285c121261612baec9
Théo Guerin <theo.guerin.pro@gmail.com>
2015-04-16 23:12:13 +0000

    ajout des librairies html/css/js bootstrap, jquery et toastr

---
+++ web/lib/toastr/config.js
@@ -0,0 +1,17 @@
+toastr.options = {
+  "closeButton": true,
+  "debug": false,
+  "newestOnTop": false,
+  "progressBar": false,
+  "positionClass": "toast-top-right",
+  "preventDuplicates": false,
+  "onclick": null,
+  "showDuration": "5000",
+  "hideDuration": "5000",
+  "timeOut": "10000",
+  "extendedTimeOut": "1000",
+  "showEasing": "swing",
+  "hideEasing": "linear",
+  "showMethod": "fadeIn",
+  "hideMethod": "fadeOut"
+}
