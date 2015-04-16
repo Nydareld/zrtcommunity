b7a4eb5e0719ae0703db5e1c04e31234e698879c
Théo Guerin <theo.guerin.pro@gmail.com>
2015-04-16 23:10:54 +0000

    création du controleur vers l'index qui redirige temporairement vers le layout de base

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,9 +5,11 @@ namespace Pecheocoup\Controller;
 use Silex\Application;
 
 class HomeController{
-
-  function indexAction(Application $app){
-    return "zbra";
-  }
+    function indexAction(Application $app){
+        return $app['twig']->render( "layout.html",array(
+            'title' => "accueil",
+            )
+        );
+    }
 
 }
