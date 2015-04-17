a2510fd2c36515485e5fb7906d99a0365ecdcd8d
Théo Guerin <theo.guerin.pro@gmail.com>
2015-04-17 00:14:29 +0000

    register du module de sécurité de symfony

--- app/app.php
+++ app/app.php
@@ -13,8 +13,25 @@ $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__.'/../views',
 ));
 
-//  === doctrine orm===
+//  === doctrine orm ===
 use Doctrine\ORM\Tools\Setup;
 use Doctrine\ORM\EntityManager;
 $config = Setup::createAnnotationMetadataConfiguration(array(__DIR__."/../src/Domain"), $app['debug']);
 $app['orm.em'] = EntityManager::create($app['db.options'], $config);
+
+//  === symfony security ===
+$app->register(new Silex\Provider\SessionServiceProvider());
+$app->register(new Silex\Provider\UrlGeneratorServiceProvider());
+$app->register(new Silex\Provider\SecurityServiceProvider(), array(
+    'security.firewalls' => array(
+        'secured' => array(
+            'pattern' => '^/',
+            'anonymous' => true,
+            'logout' => true,
+            'form' => array('login_path' => '/login', 'check_path' => '/login_check'),
+            'users' => $app->share(function () use ($app) {
+                return new MicroCMS\Domain\UserDAO($app['db']);
+            }),
+        ),
+    ),
+));
