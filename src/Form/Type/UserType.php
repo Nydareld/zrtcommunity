f65a48010b4faee5e59f9405be94c3aaee8173d9
Théo Guerin <theo.guerin.pro@gmail.com>
2015-04-17 02:07:14 +0000

    création d'un formulaire d'inscription

---
+++ src/Form/Type/UserType.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Pecheocoup\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class UserType extends AbstractType{
+    public function buildForm(FormBuilderInterface $builder, array $options){
+        $builder
+        ->add('username', 'text')
+        ->add('password', 'repeated', array(
+            'type'            => 'password',
+            'invalid_message' => 'Les mots de passes doivent etre identitques',
+            'options'         => array('required' => true),
+            'first_options'   => array('label' => 'Mot de passe'),
+            'second_options'  => array('label' => 'repetez votre mot de passe'),
+        ))
+        ->add('email', 'repeated', array(
+            'type'            => 'email',
+            'invalid_message' => 'Les emails doivent etre identitques',
+            'options'         => array('required' => true),
+            'first_options'   => array('label' => 'email'),
+            'second_options'  => array('label' => 'repetez votre email'),
+        ))
+        ->add('role', 'hidden', array(
+            'ROLE_USER' => 'User')
+        ));
+}
+
+    public function getName()
+    {
+        return 'user';
+    }
+}
