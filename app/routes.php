fc731a773332593ef659c10bd5970a01416a7365
Théo Guerin <theo.guerin.pro@gmail.com>
2015-04-17 02:40:19 +0000

    creation de la route d'inscriptions

--- app/routes.php
+++ app/routes.php
@@ -5,4 +5,12 @@ Page d'accueil
 */
 $app->get('/',"Pecheocoup\Controller\HomeController::indexAction");
 
+/*
+Page de connexion
+*/
 $app->get('/login',"Pecheocoup\Controller\HomeController::loginAction")->bind('login');
+
+/*
+Page d'inscription
+*/
+$app->match('/inscription', "Pecheocoup\Controller\UserController::addUserAction")->bind('inscription');
