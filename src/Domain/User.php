59b2002ddf1634bec97e5f027bbd0fb5c474ad5e
Théo Guerin <theo.guerin.pro@gmail.com>
2015-04-17 00:16:14 +0000

    création de la classe User qui implémente la UserInterface de symfony pour plus de facilité avec la sécurité

---
+++ src/Domain/User.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace Pecheocoup\Domain;
+
+use Symfony\Component\Security\Core\User\UserInterface;
+
+/**
+ * @Entity @Table(name="users")
+ **/
+class User implements UserInterface
+{
+    /**
+     * @Id @GeneratedValue @Column(type="integer")
+     * @var int
+     **/
+    private $id;
+
+    /**
+     * @Column(type="string")
+     * @var string
+     **/
+    private $username;
+
+    /**
+     * @Column(type="string")
+     * @var string
+     */
+    private $password;
+
+    /**
+     * @Column(type="string")
+     * @var string
+     */
+    private $salt;
+
+    /**
+     * @Column(type="string")
+     * @var string
+     */
+    private $role;
+
+    public function getId() {
+        return $this->id;
+    }
+
+    public function setId($id) {
+        $this->id = $id;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getUsername() {
+        return $this->username;
+    }
+
+    public function setUsername($username) {
+        $this->username = $username;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getPassword() {
+        return $this->password;
+    }
+
+    public function setPassword($password) {
+        $this->password = $password;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getSalt()
+    {
+        return $this->salt;
+    }
+
+    public function setSalt($salt)
+    {
+        $this->salt = $salt;
+    }
+
+    public function getRole()
+    {
+        return $this->role;
+    }
+
+    public function setRole($role) {
+        $this->role = $role;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getRoles()
+    {
+        return array($this->getRole());
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function eraseCredentials() {
+        // Nothing to do here
+    }
+}
