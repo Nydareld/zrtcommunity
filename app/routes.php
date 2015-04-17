3d2104b4ab385d8e51465a798f9a295869c4ec41
Théo Guerin <theo.guerin.pro@gmail.com>
2015-04-17 01:30:05 +0000

    création de la route login

--- app/routes.php
+++ app/routes.php
@@ -4,3 +4,5 @@
 Page d'accueil
 */
 $app->get('/',"Pecheocoup\Controller\HomeController::indexAction");
+
+$app->get('/login',"Pecheocoup\Controller\HomeController::loginAction")->bind('login');
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,14 +2,24 @@
 
 namespace Pecheocoup\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
 use Silex\Application;
 
 class HomeController{
-    function indexAction(Application $app){
+
+    public function indexAction(Application $app){
         return $app['twig']->render( "layout.html",array(
             'title' => "accueil",
             )
         );
+    }
+
+    public function loginAction(Request $request, Application $app) {
+        return $app['twig']->render('login.html', array(
+            'error'         => $app['security.last_error']($request),
+            'last_username' => $app['session']->get('_security.last_username'),
+            'title' => "login",
+            ));
     }
 
 }
