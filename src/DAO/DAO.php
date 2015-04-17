f96c49c9d91dbe0654e0439cb29547d225bd50f1
Théo Guerin <theo.guerin.pro@gmail.com>
2015-04-17 00:15:12 +0000

    création d'une classe abstraite DAO

---
+++ src/DAO/DAO.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Pecheocoup\DAO;
+
+use Doctrine\ORM\EntityManager;
+
+abstract class DAO
+{
+    /**
+     * Database connection
+     *
+     * @var \Doctrine\ORM\EntityManager
+     */
+    private $em;
+
+    /**
+     * Constructor
+     *
+     * @param \Doctrine\ORM\EntityManager The entity manager
+     */
+    public function __construct(EntityManager $em) {
+        $this->em = $em;
+    }
+
+    /**
+     * Grants access to the database connection object
+     *
+     * @return \Doctrine\ORM\EntityManager The entity manager
+     */
+    protected function getDb() {
+        return $this->em;
+    }
+
+
+}
