3384bf4f88e6c15419e20dbdf8dbdd8792477e4b
Théo Guerin <theo.guerin.pro@gmail.com>
2015-04-17 02:43:17 +0000

    création du controller des utilisateur et de l'acction adduser anisi que le template associé

---
+++ src/Controller/UserController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Pecheocoup\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Pecheocoup\Domain\User;
+use Silex\Application;
+use Pecheocoup\Form\Type\UserType;
+
+class UserController{
+
+    public function addUserAction(Request $request, Application $app) {
+
+        $user = new User();
+        $userForm = $app['form.factory']->create(new UserType(), $user);
+        $userForm->handleRequest($request);
+        if ($userForm->isSubmitted() && $userForm->isValid()) {
+            // generate a random salt value
+            $salt = substr(md5(time()), 0, 23);
+            $user->setSalt($salt);
+            $plainPassword = $user->getPassword();
+            // find the default encoder
+            $encoder = $app['security.encoder.digest'];
+            // compute the encoded password
+            $password = $encoder->encodePassword($plainPassword, $user->getSalt());
+            $user->setPassword($password);
+            $app['dao.user']->save($user);
+        }
+        return $app['twig']->render('register.html', array(
+            'title' => 'Inscription',
+            'userForm' => $userForm->createView()));
+    }
+
+}
