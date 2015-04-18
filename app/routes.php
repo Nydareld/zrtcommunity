2067533434daffa63467fe0d1969a7068f704ffa
Théo Guerin <theo.guerin.pro@gmail.com>
2015-04-18 00:37:03 +0000

    ajout de la route de la liste des membres

--- app/routes.php
+++ app/routes.php
@@ -1,16 +1,13 @@
 <?php
 
-/*
-Page d'accueil
-*/
+//Page d'accueil
 $app->get('/',"Pecheocoup\Controller\HomeController::indexAction");
 
-/*
-Page de connexion
-*/
+//Page de connexion
 $app->get('/login',"Pecheocoup\Controller\HomeController::loginAction")->bind('login');
 
-/*
-Page d'inscription
-*/
+//Page d'inscription
 $app->match('/inscription', "Pecheocoup\Controller\UserController::addUserAction")->bind('inscription');
+
+//liste des membres
+$app->match('/membres', "Pecheocoup\Controller\UserController::usersAction");
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -23,4 +23,6 @@ class HomeController{
             ));
     }
 
+    public function usersAction
+
 }
