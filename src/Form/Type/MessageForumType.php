ce7e3d7ca03d1416b10838c5891914ea165354c1
Théo Guerin <theo.guerin.pro@gmail.com>
2015-04-29 22:50:36 +0000

    création du formulaire de post de message forum

---
+++ src/Form/Type/MessageForumType.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Zrtcommunity\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class MessageForumType extends AbstractType{
+
+    public function buildForm(FormBuilderInterface $builder, array $options){
+        $builder
+        ->add('contenu', 'textarea',array(
+            'label' => 'Répondez!',
+            'max_length' => 5000,
+            'attr'=>array('class'=>'ckeditor')
+        ))
+        ->add('save', 'submit', array(
+            'label' => 'Envoyer',
+            'attr'=>array('class' => 'gauche')
+        ));
+    }
+
+    public function getName()
+    {
+        return 'messageforum';
+    }
+}
