baa912ac7eb783d2e3c598a517f14eadce025920
Théo Guerin <theo.guerin.pro@gmail.com>
2015-04-30 17:36:20 +0000

    ajout d'une fonction de recherche de messages

--- src/DAO/MessageForumDAO.php
+++ src/DAO/MessageForumDAO.php
@@ -9,4 +9,13 @@ class MessageForumDAO extends DAO{
         $this->getEm()->persist($mess);
         $this->getEm()->flush();
     }
+    public function loadMessageById($id){
+        $mess= $this->getEm()->getRepository('Zrtcommunity\Domain\MessageForum')->findOneBy(array('id' => $id));
+
+        if ($mess === null){
+            throw new \Exception("No topic matching id " . $id);
+        }else{
+            return $mess;
+        }
+    }
 }
