9c2a7d84569a0537b2a0f195bd6cbf6a1fb118ee
Théo Guerin <theo.guerin.pro@gmail.com>
2015-05-02 00:36:56 +0000

    intégration de kcfinder (gestionaire de fichiers) a ckeditor full pour stoquer les images des news sur le forum

--- web/lib/ckeditor-full/config.js
+++ web/lib/ckeditor-full/config.js
@@ -7,4 +7,12 @@ CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
+	// ...
+   config.filebrowserBrowseUrl = '/lib/kcfinder-master/browse.php?opener=ckeditor&type=files';
+   config.filebrowserFlashBrowseUrl = '/lib/kcfinder-master/browse.php?opener=ckeditor&type=flash';
+   config.filebrowserImageBrowseUrl = '/lib/kcfinder-master/browse.php?opener=ckeditor&type=images';
+   config.filebrowserUploadUrl = '/lib/kcfinder-master/upload.php?opener=ckeditor&type=files';
+   config.filebrowserFlashUploadUrl = '/lib/kcfinder-master/upload.php?opener=ckeditor&type=flash';
+   config.filebrowserImageUploadUrl = '/lib/kcfinder-master/upload.php?opener=ckeditor&type=images';
+// ...
 };
