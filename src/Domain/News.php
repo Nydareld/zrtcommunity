94560acb0daea735dcb60d8d9067443ab09a74c0
Théo Guerin <theo.guerin.pro@gmail.com>
2015-05-02 00:45:14 +0000

    ajout d'un POPO pour les news

---
+++ src/Domain/News.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace Zrtcommunity\Domain;
+
+/**
+ * @Entity @Table(name="news")
+ **/
+class News{
+    /**
+     * @Id @GeneratedValue @Column(type="integer")
+     * @var int
+     */
+    private $id;
+    /**
+     * @Column(type="string")
+     * @var string
+     */
+    private $name;
+    /**
+     * @Column(type="datetime")
+     * @var datetime
+     */
+    private $date;
+    /**
+     * @Column(type="string",length=5000)
+     * @var string
+     */
+    private $contenu;
+    /**
+     * @ManyToOne(targetEntity="User", inversedBy="news")
+     */
+    private $creator;
+    /**
+     * @OneToMany(targetEntity="MessageNews", mappedBy="news")
+     */
+    protected $messages;
+
+    public function getId(){
+		return $this->id;
+	}
+
+	public function setId($id){
+		$this->id = $id;
+	}
+
+	public function getName(){
+		return $this->name;
+	}
+
+	public function setName($name){
+		$this->name = $name;
+	}
+
+	public function getDate(){
+		return $this->date;
+	}
+
+	public function setDate($date){
+		$this->date = $date;
+	}
+
+	public function getContenu(){
+		return $this->contenu;
+	}
+
+	public function setContenu($contenu){
+		$this->contenu = $contenu;
+	}
+
+	public function getCreator(){
+		return $this->creator;
+	}
+
+	public function setCreator($creator){
+		$this->creator = $creator;
+	}
+
+	public function getMessages(){
+		return $this->messages;
+	}
+
+	public function setMessages($messages){
+		$this->messages = $messages;
+	}
+}
