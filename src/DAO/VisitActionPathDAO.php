c4e5b0e2541fcd26e6f4efb277f138eb3f0ab349
Théo Guerin <theo.guerin.pro@gmail.com>
2015-05-03 23:47:35 +0000

    renommage de vap DAO

--- src/DAO/VisitActionPathDAO.php
+++ src/DAO/VisitActionPathDAO.php
@@ -6,7 +6,7 @@ use Zrtcommunity\Domain\VisitActionPath;
 
 class VisitActionPathDAO extends DAO{
     public function save(VisitActionPath $visitActionPath){
-        $this->getEm()->persist($mess);
+        $this->getEm()->persist($visitActionPath);
         $this->getEm()->flush();
     }
 }
