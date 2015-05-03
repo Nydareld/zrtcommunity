57dde6626fd37c0c92e968d80beca8c7f99c65b9
Théo Guerin <theo.guerin.pro@gmail.com>
2015-05-03 23:48:09 +0000

    implémentation d'une fonction de recherche par ip et par date dans visitDAO

--- src/DAO/VisitDAO.php
+++ src/DAO/VisitDAO.php
@@ -5,8 +5,23 @@ namespace Zrtcommunity\DAO;
 use Zrtcommunity\Domain\Visit;
 
 class VisitDAO extends DAO{
+
     public function save(Visit $visit){
         $this->getEm()->persist($visit);
         $this->getEm()->flush();
     }
+
+    public function loadCurrentVisit( $ip, $date){
+        $visit = $this->getEm()->getRepository('Zrtcommunity\Domain\Visit')
+            ->findOneBy(array(
+                'ip'=>$ip,
+                'date'=>$date,
+            ));
+        if ($visit === null){
+            return null;
+        }else{
+            return $visit;
+        }
+    }
+
 }
