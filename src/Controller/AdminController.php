ee9718ae96cbc07617b2681c7e67a31886e9fbc7
Théo Guerin <theo.guerin.pro@gmail.com>
2015-05-04 21:55:22 +0000

    création du controller des statisques de l'application

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -10,8 +10,26 @@ use \DateTime;
 
 class AdminController{
     public function adminAction(Request $request, Application $app){
-        return $app['twig']->render( "admin-Layout.html",array(
+
+        $days = array();
+        for ($i = 0; $i < 30; $i++) {
+            $days[] = new DateTime(date('Y-m-d',strtotime('-'.$i.'days')));
+        }
+
+        $visits=$app['dao.visit']->findAll();
+
+        $nbVisitsUser=array();
+        $nbVisitsGuest=array();
+
+        for($i =0; $i<count($days);$i++){
+            $nbVisitsUser[$i]=$app['dao.visit']->findUserVisitByDay($days[$i]);
+            $nbVisitsGuest[$i]=$app['dao.visit']->findGuestVisitByDay($days[$i]);
+        }
+        return $app['twig']->render( "admin-stat.html",array(
             'panelname' => 'statistiques',
+            'visitsUser' => $nbVisitsUser,
+            'visitsGuest' => $nbVisitsGuest,
+            'days'=>$days,
             )
         );
     }
