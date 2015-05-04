948f41a1caf04052f6ab013c2e90fde02be8dbe7
Théo Guerin <theo.guerin.pro@gmail.com>
2015-05-04 22:06:52 +0000

    Reverse du tableau des visites

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -15,6 +15,7 @@ class AdminController{
         for ($i = 0; $i < 30; $i++) {
             $days[] = new DateTime(date('Y-m-d',strtotime('-'.$i.'days')));
         }
+        $days=array_reverse($days);
 
         $visits=$app['dao.visit']->findAll();
 
@@ -25,6 +26,8 @@ class AdminController{
             $nbVisitsUser[$i]=$app['dao.visit']->findUserVisitByDay($days[$i]);
             $nbVisitsGuest[$i]=$app['dao.visit']->findGuestVisitByDay($days[$i]);
         }
+
+
         return $app['twig']->render( "admin-stat.html",array(
             'panelname' => 'statistiques',
             'visitsUser' => $nbVisitsUser,
