d13b7a09e4d64c51d3062b28d904112be82e040d
Théo Guerin <theo.guerin.pro@gmail.com>
2015-05-05 14:23:13 +0000

    Création du POPO Region

---
+++ src/Domain/Region.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Zrtcommunity\Domain;
+
+/**
+ * @Entity @Table(name="region")
+ **/
+class Region{
+
+    /**
+     * @Id @GeneratedValue @Column(type="integer")
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @Column(type="string")
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @Column(type="string",length=10000)
+     * @var string
+     */
+    private $description;
+
+    /**
+     * @OneToMany(targetEntity="Projet", mappedBy="region")
+     */
+    private $projets;
+
+    public function getId(){
+		return $this->id;
+	}
+
+	public function setId($id){
+		$this->id = $id;
+	}
+
+	public function getName(){
+		return $this->name;
+	}
+
+	public function setName($name){
+		$this->name = $name;
+	}
+
+	public function getDescription(){
+		return $this->description;
+	}
+
+	public function setDescription($description){
+		$this->description = $description;
+	}
+
+	public function getProjets(){
+		return $this->projets;
+	}
+
+	public function setProjets($projets){
+		$this->projets = $projets;
+	}
+}
