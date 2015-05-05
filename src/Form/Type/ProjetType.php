6863f24c6e2c307112ad95bf738eeddbf6ec17c7
Théo Guerin <theo.guerin.pro@gmail.com>
2015-05-05 22:06:57 +0000

    création d'un formulaire pour un projet

---
+++ src/Form/Type/ProjetType.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Zrtcommunity\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+
+class ProjetType extends AbstractType{
+
+    public function buildForm(FormBuilderInterface $builder, array $options){
+        global $app;
+
+        $regions=$app['dao.region']->findAllNamesAsArray();
+
+        $builder
+        ->add('name', 'text',array(
+            'label' => 'Nom du projet:'
+        ))
+        ->add('description', 'textarea',array(
+            'label' => 'Description:',
+            'max_length' => 10000,
+            'attr'=>array('class'=>'ckeditor')
+        ))
+        ->add('region','choice',array(
+            'mapped' => false,
+            'choices'=>$regions
+        ))
+        ->add('save', 'submit', array(
+            'label' => 'Envoyer',
+            'attr'=>array('class' => 'droite')
+        ));
+    }
+
+    public function getName()
+    {
+        return 'projet';
+    }
+}
