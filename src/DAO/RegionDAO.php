40994b449f95dc2ecfb734bf6ec2c36e2ecc54e2
Théo Guerin <theo.guerin.pro@gmail.com>
2015-05-05 22:03:44 +0000

    ajout de fonctions de recherche de région*

--- src/DAO/RegionDAO.php
+++ src/DAO/RegionDAO.php
@@ -9,4 +9,28 @@ class RegionDAO extends DAO{
         $this->getEm()->persist($region);
         $this->getEm()->flush();
     }
+    public function findAllNamesAsArray(){
+        $names = array();
+
+        $regions= $this->getEm()->getRepository('Zrtcommunity\Domain\Region')->findAll();
+
+        if ($regions === null){
+            throw new \Exception("No Regions");
+        }
+
+        foreach($regions as $region){
+            $names[$region->getId()] = $region->getName();
+        }
+
+        return $names;
+    }
+    public function loadRegionById($id){
+        $news= $this->getEm()->getRepository('Zrtcommunity\Domain\Region')->findOneBy(array('id' => $id));
+
+        if ($news === null){
+            throw new \Exception("No region matching id " . $id);
+        }else{
+            return $news;
+        }
+    }
 }
