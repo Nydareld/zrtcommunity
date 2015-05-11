0fcefc206a10f715010c2f7ce22e2ac51e9dc48c
Théo Guerin <theo.guerin.pro@gmail.com>
2015-05-11 23:06:05 +0000

    ajout de la signature au formulaire de modification de profile

--- src/Form/Type/ProfileType.php
+++ src/Form/Type/ProfileType.php
@@ -10,7 +10,13 @@ class ProfileType extends AbstractType{
     public function buildForm(FormBuilderInterface $builder, array $options){
         $builder
         ->add('avatar','file',array(
-            'required'=>'false',
+            'required'=>false
+        ))
+        ->add('sign', 'textarea',array(
+            'required'=>false,
+            'label' => 'Signature(1000 caracteres):',
+            'max_length' => 1000,
+            'attr'=>array('class'=>'ckeditor')
         ))
         ->add('save', 'submit', array(
             'label' => 'Envoyer',
