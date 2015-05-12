94ff2561cb86228916dcc1d64f4fa18e5efb3ac5
Théo Guerin <theo.guerin.pro@gmail.com>
2015-05-12 11:44:38 +0000

    form de catégorie

---
+++ src/Form/Type/CategorieType.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Zrtcommunity\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class CategorieType extends AbstractType{
+
+    public function buildForm(FormBuilderInterface $builder, array $options){
+        global $app;
+
+        $builder
+        ->add('name', 'text',array(
+            'label' => 'Nom nouvelle catégorie:'
+        ))
+        ->add('save', 'submit', array(
+            'label' => 'Envoyer'
+        ));
+    }
+
+    public function getName()
+    {
+        return 'categorie';
+    }
+}
