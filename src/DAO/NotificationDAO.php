33cb21c8814ab4bcaa1d78ff0664ae7e907455d9
Théo Guerin <theo.guerin.pro@gmail.com>
2015-05-14 15:33:39 +0000

    dao d'une notification

---
+++ src/DAO/NotificationDAO.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Zrtcommunity\DAO;
+
+use Zrtcommunity\Domain\Notification;
+
+class NotificationDAO extends DAO{
+    public function save(Notification $notif){
+        $this->getEm()->persist($notif);
+        $this->getEm()->flush();
+    }
+
+    public function remove(Notification $notif){
+        $this->getEm()->remove($notif);
+        $this->getEm()->flush();
+    }
+
+    public function loadNotifById($id){
+        $news= $this->getEm()->getRepository('Zrtcommunity\Domain\Notification')->findOneBy(array('id' => $id));
+
+        if ($news === null){
+            return null;
+        }else{
+            return $news;
+        }
+    }
+
+}
